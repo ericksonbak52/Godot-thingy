a8a792f2eba51d36b070be1ba94d44a6f5fdfbc0
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2025-01-15 23:00:20 -0500

    new things

--- Scenes/bacon_portal.gd
+++ Scenes/bacon_portal.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("joe")
+	queue_free()
