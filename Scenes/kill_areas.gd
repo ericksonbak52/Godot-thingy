4c0313eefa837c20f92bc455d2100d9adcc6317b
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2025-01-25 23:11:02 -0500

    making death kinda work

---
+++ Scenes/kill_areas.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var player = $"Player"
+
+func _on_body_entered(body: Node2D) -> void:
+	death()
+
+func death():
+	print("dead")
--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+
+@onready var killScriptLoad = preload("res://Scenes/kill_areas.gd")
+@onready var killScript = killScriptLoad.new()
 
 
 func _physics_process(delta: float) -> void:
@@ -23,3 +25,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		if collision.get_collider().name == "deathTiles":
+			killScript.death()
