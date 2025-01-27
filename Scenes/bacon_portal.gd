8fbdc16b35077ee021cc0f010e5357b7350bcaf4
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2025-01-26 22:02:26 -0500

    change

--- Scenes/bacon_portal.gd
+++ Scenes/bacon_portal.gd
@@ -3,7 +3,9 @@ extends Sprite2D
 @onready var cam = $"../../Camera2D"
 @onready var target
 @onready var current
-var speed
+@onready var portalAnimator = $"../../portalAnimator"
+var speed = 6
+@onready var pos = position
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current = cam.position
@@ -12,10 +14,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	current = cam.position
-	speed = 6
 	cam.position = current.lerp(target, speed * delta)
+	position = pos
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	pos = position + Vector2(290, 0)
 	target = target + Vector2(290, 0)
 	print("target good")
+	
 	#queue_free()
