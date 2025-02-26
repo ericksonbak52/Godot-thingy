049203b9c1046a3eebf82c9bee00728d0de93fb9
ericksonbak52 <89548995+ericksonbak52@users.noreply.github.com>
2025-02-25 23:55:02 -0500

    k

--- Scripts/bacon_portal.gd
+++ Scripts/bacon_portal.gd
@@ -1,9 +1,8 @@
-extends Sprite2D
+extends Area2D 
 
 @onready var cam = $"../../Cam"
 @onready var spawnPoint = $"../../Cam/spawnPoint"
 @onready var player = $"../../Player"
-@onready var portalAnimator = get_parent()
 @onready var target
 @onready var current
 @onready var pos = position
@@ -13,8 +12,6 @@ var speed = 6
 func _ready() -> void:
 	current = cam.position
 	target = current
-	portalAnimator.play("portal_float");
-	#$"../../joe".play("portalFloat")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,9 +19,9 @@ func _process(delta: float) -> void:
 	cam.position = current.lerp(target, speed * delta)
 	position = pos
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
+#when things on mask layer wo hit the thing
+func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	print(spawnPoint)
 	pos = position + Vector2(290, 0) 
 	target = target + Vector2(290, 0)
 	player.position = spawnPoint.position
-	
